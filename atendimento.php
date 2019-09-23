f36c8a549e9c5acf4b7d9ab39f7e798ffd25d23f
Hiago Souza <hiagofss98@gmail.com>
2019-09-23 19:53:57 -0300

    Init project

---
+++ atendimento.php
@@ -0,0 +1,39 @@
+<?php require_once "header.html" ?>
+<header>
+  <div class="container text-left mt-3">
+    <h1 class="cover-heading">Atendimentos</h1>
+    <button type="button" class="btn btn-primary mt-3 mb-5">Novo atendimento</button>
+    <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td><button type="button" class="btn btn-secondary">Detalhar</button>    </td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+
+</header>
+
+<?php require_once "footer.html" ?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require_once "header.html" ?>
+<header id="home" class="">
+    
+</header>
+
+<?php require_once "footer.html" ?>
